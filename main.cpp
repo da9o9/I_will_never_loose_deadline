33ac6399479e137df3fe2e3d273968415274d424
da9o9 <egor.dudko2004@gmail.com>
2024-06-04 19:36:37 +0300

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+
+    for (int i = 0; i < num; ++i) {
+        std::cout << "I will never lose deadline!" << std::endl;
+    }
+
+    return 0;
+}
